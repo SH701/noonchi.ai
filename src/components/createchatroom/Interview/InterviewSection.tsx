1a2d1a11ed71c71dcd0771dbae537bebada5d1b4
KSH701 <rkrk7636@naver.com>
2026-02-09 19:18:41 +0900

    Fix: 인터뷰 섹션 props 오류 해결

--- src/components/createchatroom/Interview/InterviewSection.tsx
+++ src/components/createchatroom/Interview/InterviewSection.tsx
@@ -1,15 +1,13 @@
-import { Topics } from "@/types/topics";
+import { TopicRes } from "@/types/topics";
 import Image from "next/image";
 
-interface Props {
-  topic: Topics;
-}
 
-export default function InterviewSection({ topic }: Props) {
+
+export default function InterviewSection({ imageUrl,name,category,description }: TopicRes) {
   return (
     <div className="relative w-full aspect-square max-w-83.75 mx-auto">
       <Image
-        src={topic?.img || "/default-image.jpg"}
+        src={imageUrl || "/default-image.jpg"}
         alt="topic's photo"
         fill
         className="object-cover rounded-3xl"
@@ -17,12 +15,12 @@ export default function InterviewSection({ topic }: Props) {
       <div className="" />
       <div className="absolute top-4 left-4">
         <span className="text-gray-600 px-3 py-2 bg-white/50 text-sm  rounded-3xl border border-gray-200">
-          {topic?.topic}
+          {name}
         </span>
       </div>
       <div className="absolute inset-x-0 bottom-0 h-auto bg-gray backdrop-blur-sm rounded-b-3xl flex flex-col p-4 text-white">
-        <span className="text-3xl font-semibold">{topic?.title}</span>
-        <span className="text-sm font-medium">{topic?.description}</span>
+        <span className="text-3xl font-semibold">{category}</span>
+        <span className="text-sm font-medium">{description}</span>
       </div>
     </div>
   );
