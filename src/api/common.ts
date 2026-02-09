a96f1ba9d698ac40aedc7e317fcbbca8def60c03
KSH701 <rkrk7636@naver.com>
2026-02-09 22:48:09 +0900

    Fix: 인증 세션 로딩 전 데이터 페칭이 발생하는 이슈 수정

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import { getSession } from "next-auth/react";
 
 export const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
@@ -6,3 +7,15 @@ export const axios = Axios.create({
     "Content-Type": "application/json",
   },
 });
+axios.interceptors.request.use(
+  async (config) => {
+    const session = await getSession();
+    if (session && session.accessToken) {
+      config.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
